c12d0d47191f8564527742788a40de2ca7236f3f
haris <634234024@parichat.skru.ac.th>
2022-08-22 09:06:54 +0700

    update Port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var cors = require("cors");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(
